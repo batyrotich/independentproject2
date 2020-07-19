d34f134c39693b996a2cf69c51a47eba99f9363f
eddah <eddahbaty98@gmail.com>
2020-07-19 12:44:11 +0300

    add functions to index.js

--- js/index.js
+++ js/index.js
@@ -1,17 +1,34 @@
+//declare arrays for akan names
 var weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 var maleAkanNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
 
 var femaleAkanNames = ['Akosua', 'Adwao', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
-
+//calling functions
 function getAkanName() {
-
 var day = parseInt(document.getElementById("day").Value);
-var month =  parseInt(document.getElementById("month").Value);
-var year =  parseInt(document.getElementById("year").Value);
+var month = parseInt(document.getElementById("month").Value);
+var year = parseInt(document.getElementById("year").Value);
 
-if(day < 1 || day > 31){
-    alert("Invalid day")
+// validate day
+if(day <= 1 || day >= 32){
+    alert(
+        "Incorrect day entry! Try again"
+  );
 }
+// validate month
+if(month <= 0 || month >= 13){
+    alert(
+        "Incorrect month entry! Try Again"
+    );
+}
+// validate year
+if( year <= 0 || year > 2020){
+   alert(
+       "Incorrect year entry! Try again"
+   );
+}
+
+var day = new Date(day + "/" + month + "/" + year);
 
 }
